6b64ffc16cc4fa2d5d383377cf1f71072d042732
Yann Jorelle <yannjorelle@protonmail.com>
2020-12-01 12:53:05 +0200

    Fix TLS errors in votecount command

--- commands/votecount.js
+++ commands/votecount.js
@@ -1,5 +1,6 @@
 const axios = require("axios").default;
 const { MessageEmbed } = require("discord.js");
+const { Agent } = require("https");
 
 module.exports = {
   name: "votecount",
@@ -8,8 +9,11 @@ module.exports = {
   description: "Counts the amount of votes",
   execute: async (message) => {
     try {
-      const { data } = await axios.get("https://i.tf.fi/voteresults");
-      const heigts = data
+      // Custom agent to ignore TLS errors
+      const { data } = await axios.get("https://i.tf.fi/voteresults", {
+        httpsAgent: new Agent({ rejectUnauthorized: false })
+      });
+      const heights = data
         .match(/(?<=height: )\d*(?=px; )/g)
         .map((height) => Math.floor(height / 4));
 
@@ -17,12 +21,13 @@ module.exports = {
         .setColor("#e8ba04")
         .setTitle("Vote results")
         .addFields(
-          { name: "Orange", value: heigts[0], inline: true },
-          { name: "Blue",   value: heigts[1], inline: true },
-          { name: "Green",  value: heigts[2], inline: true }
+          { name: "Orange", value: heights[0], inline: true },
+          { name: "Blue",   value: heights[1], inline: true },
+          { name: "Green",  value: heights[2], inline: true }
         );
       message.channel.send(embed);
     } catch (exception) {
+      message.channel.send("Failed while fetching vote count");
       console.error(exception);
     }
   }
